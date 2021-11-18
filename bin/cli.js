5dcb5444765f65d8a1a7930bf3916ce19e66edad
yearthmain <yearthmain@gmail.com>
2021-11-18 22:10:18 +0800

    [feat]: 新增欢迎界面

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 import program from "commander";
 import require from "../shared/require.js";
+import init from "../lib/init.js";
 
 program.version(require("../package.json").version);
 
-program
-  .command("init <name>")
-  .description("init project")
-  .action(name => console.log("hello commander", name));
+program.command("init <name>").description("init project").action(init);
 
 program.parse(process.argv);
