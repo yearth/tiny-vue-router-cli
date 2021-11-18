2507c5305e4cdcec109cde68e7623147f26e8758
yearthmain <yearthmain@gmail.com>
2021-11-18 22:00:39 +0800

    [feat]: 添加 cli 入口

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
+import program from "commander";
+import require from "../shared/require.js";
 
-console.log("hello world");
+program.version(require("../package.json").version);
+
+program
+  .command("init <name>")
+  .description("init project")
+  .action(name => console.log("hello commander", name));
+
+program.parse(process.argv);
