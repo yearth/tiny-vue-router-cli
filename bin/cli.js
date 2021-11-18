3c95f543b874d2755c557eceb15cecf181cd112b
yearthmain <yearthmain@gmail.com>
2021-11-18 23:19:47 +0800

    [feat]: 新增自动根据 views 下的组件更新路由

--- bin/cli.js
+++ bin/cli.js
@@ -2,9 +2,11 @@
 import program from "commander";
 import require from "../shared/require.js";
 import init from "../lib/init.js";
+import refresh from "../lib/refresh.js";
 
 program.version(require("../package.json").version);
 
 program.command("init <name>").description("init project").action(init);
+program.command("refresh").description("refresh router").action(refresh);
 
 program.parse(process.argv);
